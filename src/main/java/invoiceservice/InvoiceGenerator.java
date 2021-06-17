31178938aa6fcd5a8df526e1804cc962ff77209d
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-06-17 17:03:06 +0530

    Calculated: toatal fare for multiple rides

--- src/main/java/invoiceservice/InvoiceGenerator.java
+++ src/main/java/invoiceservice/InvoiceGenerator.java
@@ -1,11 +1,35 @@
 package invoiceservice;
 
 public class InvoiceGenerator {
+    /**
+     * declaration of the constants
+     */
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_TIME = 1;
+    private static final int MINIMUM_FARE = 5;
 
+    /**
+     * this is the method to calculate the total fare for single ride when given distance and time;
+     * @param distance;
+     * @param time;
+     * @return double;
+     */
     public double calculateFare(double distance, int time) {
-        return distance*MINIMUM_COST_PER_KILOMETER+time*COST_PER_TIME;
+        double totalFare = distance*MINIMUM_COST_PER_KILOMETER+time*COST_PER_TIME;
+        return Math.max(totalFare, MINIMUM_FARE);
+    }
+
+    /**
+     * this is the method to calculate total fare for multiple rides.
+     * @param rides;
+     * @return double;
+     */
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride:rides) {
+            totalFare += this.calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return totalFare;
     }
 }
 
--- src/test/java/invoiceservice/InvoiceServiceTest.java
+++ src/test/java/invoiceservice/InvoiceServiceTest.java
@@ -1,18 +1,44 @@
 package invoiceservice;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceServiceTest {
 
+    InvoiceGenerator invoiceGenerator = null;
+
+    @BeforeEach
+    void setUp() {
+        invoiceGenerator = new InvoiceGenerator();
+    }
+
     @DisplayName("this is test case to check total fare when given Distance and time")
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(25, fare, 0.0);
+    }
+
+    @DisplayName("this is the test to check to return minimum fare when given less distance and time")
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(5, fare);
+    }
+
+    @DisplayName("this is the test case to check total fare for multiple rides")
+    @Test
+    public void givenMultipleRides_shouldReturnTotalFare() {
+        Ride[] rides = { new Ride(2.0, 5),
+                         new Ride(0.1, 1)
+                        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assertions.assertEquals(30, fare, 0.0);
     }
 }
